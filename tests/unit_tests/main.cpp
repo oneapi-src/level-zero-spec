e393a824746c857fb3a67150ba77b89709a18b44
Brandon Fliflet <brandon.fliflet@intel.com>
2019-01-26 21:10:27 -0800

    Adding optional global environment initialization for ULTs

--- tests/unit_tests/main.cpp
+++ tests/unit_tests/main.cpp
@@ -4,8 +4,14 @@ TEST(Should, pass) {
     EXPECT_TRUE(true);
 }
 
+::testing::Environment *environment = nullptr;
+
 int main(int argc, char **argv) {
     testing::InitGoogleMock(&argc, argv);
+
+    if (environment) {
+        ::testing::AddGlobalTestEnvironment(environment);    
+    }
 
     return RUN_ALL_TESTS();
 }
