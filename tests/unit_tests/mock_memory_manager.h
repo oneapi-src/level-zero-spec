4c93249c3236e848ca69acab408031244b96f622
Brandon Fliflet <brandon.fliflet@intel.com>
2019-01-30 09:46:51 -0800

    Whitebox template specializations moved outside of xe namespace.

--- tests/unit_tests/mock_memory_manager.h
+++ tests/unit_tests/mock_memory_manager.h
@@ -3,11 +3,11 @@
 #include "white_box.h"
 #include "gmock/gmock.h"
 
-namespace xe {
-
 template <>
-struct WhiteBox<MemoryManager> : public MemoryManager {
+struct WhiteBox<xe::MemoryManager> : public xe::MemoryManager {
 };
+
+namespace xe {
 
 struct MockMemoryManager : public WhiteBox<MemoryManager> {
     MOCK_METHOD0(allocateDeviceMemory, GraphicsAllocation *());
