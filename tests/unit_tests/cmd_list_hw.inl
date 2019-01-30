15497089315883c3a7d8c1d0e3c12216c7195a9a
Brandon Fliflet <brandon.fliflet@intel.com>
2019-01-30 07:30:58 -0800

    Adding an uninitialized MI_SEMAPHORE_WAIT during encodeWaitOnEvent

--- tests/unit_tests/cmd_list_hw.inl
+++ tests/unit_tests/cmd_list_hw.inl
@@ -1,4 +1,5 @@
 #include "cmd_list_hw.h"
+#include "runtime/command_stream/linear_stream.h"
 #include "runtime/helpers/hw_info.h"
 
 namespace xe {
@@ -16,6 +17,12 @@ xe_result_t CommandListHw<gfxCoreFamily>::close() {
 
 template <uint32_t gfxCoreFamily>
 xe_result_t CommandListHw<gfxCoreFamily>::encodeWaitOnEvent(xe_event_handle_t hEvent) {
+    using GfxFamily = typename OCLRT::GfxFamilyMapper<static_cast<GFXCORE_FAMILY>(gfxCoreFamily)>::GfxFamily;
+    using MI_SEMAPHORE_WAIT = typename GfxFamily::MI_SEMAPHORE_WAIT;
+    MI_SEMAPHORE_WAIT cmd = {};
+    auto buffer = commandStream->getSpace(sizeof(cmd));
+    *(MI_SEMAPHORE_WAIT *)buffer = cmd;
+
     return XE_RESULT_SUCCESS;
 }
 
--- tests/unit_tests/test_cmdlist_encodewaitonevent.cpp
+++ tests/unit_tests/test_cmdlist_encodewaitonevent.cpp
@@ -1,6 +1,14 @@
 #include "xe_all.h"
+#include "graphics_allocation.h"
+#include "igfxfmid.h"
 #include "mock_cmd_list.h"
+#include "mock_device.h"
+#include "mock_event.h"
+#include "mock_memory_manager.h"
+#include "runtime/command_stream/linear_stream.h"
 #include "gmock/gmock.h"
+
+using ::testing::Return;
 
 TEST(xeCommandListEncodeWaitOnEvent, redirectsToCmdListObject) {
     xe::MockCommandList commandList;
@@ -11,4 +19,28 @@ TEST(xeCommandListEncodeWaitOnEvent, redirectsToCmdListObject) {
     auto result = xe::xeCommandListEncodeWaitOnEvent(commandList.toHandle(),
                                                      event);
     EXPECT_EQ(XE_RESULT_SUCCESS, result);
+}
+
+TEST(CommandListEncodeWaitOnEvent, addsCommandsToCommandStream) {
+    xe::MockDevice device;
+    xe::MockMemoryManager memoryManager;
+    EXPECT_CALL(device, getMemoryManager())
+        .WillRepeatedly(Return(&memoryManager));
+
+    int8_t buffer[1024];
+    xe::GraphicsAllocation allocation(buffer, sizeof(buffer));
+    EXPECT_CALL(memoryManager, allocateDeviceMemory)
+        .WillOnce(Return(&allocation));
+
+    auto commandList = xe::CommandList::create(IGFX_SKYLAKE, &device);
+    auto commandListAlias = whitebox_cast<xe::CommandList>(commandList);
+    ASSERT_NE(nullptr, commandListAlias->commandStream);
+    auto usedSpaceBefore = commandListAlias->commandStream->getUsed();
+
+    xe::MockEvent event;
+    auto result = commandList->encodeWaitOnEvent(event.toHandle());
+    ASSERT_EQ(XE_RESULT_SUCCESS, result);
+
+    auto usedSpaceAfter = commandListAlias->commandStream->getUsed();
+    EXPECT_GT(usedSpaceAfter, usedSpaceBefore);
 }
