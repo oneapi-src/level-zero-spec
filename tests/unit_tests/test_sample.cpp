d123551829241c0784c716cd0edb25d18cf99414
Brandon Fliflet <brandon.fliflet@intel.com>
2019-02-01 00:14:00 -0800

    Moving test_sample into xe namespace to exercise driver logic.

--- tests/unit_tests/test_sample.cpp
+++ tests/unit_tests/test_sample.cpp
@@ -1,47 +1,51 @@
-#include <cmath>
-#include <cstdint>
-#include "xe_all.h"
+#include "cmdlist.h"
+#include "cmdqueue.h"
+#include "driver.h"
+#include "device.h"
+#include "event.h"
 #include "gtest/gtest.h"
 
+namespace xe {
+
 TEST(sample, waitOnEvent) {
-    auto result = xeDriverInit(XE_INIT_FLAG_NONE);
+    auto result = xe::xeDriverInit(XE_INIT_FLAG_NONE);
     ASSERT_EQ(XE_RESULT_SUCCESS, result);
 
     xe_device_handle_t device = {};
-    result = xeDriverGetDevice(0,
+    result = xe::xeDriverGetDevice(0,
                                &device);
     ASSERT_EQ(XE_RESULT_SUCCESS, result);
 
     xe_command_queue_handle_t hCommandQueue = {};
     xe_command_queue_desc_t desc = {};
-    result = xeDeviceCreateCommandQueue(device,
+    result = xe::xeDeviceCreateCommandQueue(device,
                                         &desc,
                                         &hCommandQueue);
     ASSERT_EQ(XE_RESULT_SUCCESS, result);
 
     xe_command_list_desc_t descCommandList = {};
     xe_command_list_handle_t hCommandList = {};
-    result = xeDeviceCreateCommandList(device,
+    result = xe::xeDeviceCreateCommandList(device,
                                        &descCommandList,
                                        &hCommandList);
     ASSERT_EQ(XE_RESULT_SUCCESS, result);
 
     xe_event_desc_t descEvent = {};
     xe_event_handle_t hEvent = {};
-    result = xeDeviceCreateEvent(device,
+    result = xe::xeDeviceCreateEvent(device,
                                  &descEvent,
                                  &hEvent);
     ASSERT_EQ(XE_RESULT_SUCCESS, result);
 
-    result = xeCommandListEncodeWaitOnEvent(hCommandList,
+    result = xe::xeCommandListEncodeWaitOnEvent(hCommandList,
                                             hEvent);
     ASSERT_EQ(XE_RESULT_SUCCESS, result);
 
-    result = xeCommandListClose(hCommandList);
+    result = xe::xeCommandListClose(hCommandList);
     ASSERT_EQ(XE_RESULT_SUCCESS, result);
 
     xe_fence_handle_t hFence = {};
-    result = xeCommandQueueEnqueueCommandLists(hCommandQueue,
+    result = xe::xeCommandQueueEnqueueCommandLists(hCommandQueue,
                                                1,
                                                &hCommandList,
                                                (xe_fence_handle_t)0);
@@ -50,9 +54,11 @@ TEST(sample, waitOnEvent) {
     result = xeEventDestroy(hEvent);
     ASSERT_EQ(XE_RESULT_SUCCESS, result);
 
-    result = xeCommandListDestroy(hCommandList);
+    result = xe::xeCommandListDestroy(hCommandList);
     ASSERT_EQ(XE_RESULT_SUCCESS, result);
 
     result = xeCommandQueueDestroy(hCommandQueue);
     ASSERT_EQ(XE_RESULT_SUCCESS, result);
 }
+
+} // namespace xe
