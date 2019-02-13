e9cdcfd24e359ddd3df9e58f98568fa0a5ffd496
Apodaca, Michael <michael.apodaca@intel.com>
2019-02-13 14:34:53 -0800

    Update generate_api.py

--- scripts/generate_api.py
+++ scripts/generate_api.py
@@ -10,6 +10,30 @@ def generate_cmake(path, namespace, files):
         name=namespace,
         files=files)
 
+def generate_code(path, namespace, specs, type):
+    loc = 0
+    util.makePath(path)
+    util.removeFiles(path, "*%s"%type)
+    util.removeFiles(path, "*.txt")
+
+    files = []
+    for s in specs:
+        fin = os.path.join("templates", "api%s.mako"%type)
+        filename = "%s_%s%s"%(namespace, s['name'], type)
+        files.append(filename)
+        fout = os.path.join(path, filename)
+
+        print("Generating %s..."%fout)
+        loc += util.makoWrite(
+            fin, fout,
+            x=namespace,
+            X=namespace.upper(),
+            Xx=namespace.title(),
+            name = s['name'],
+            header = s['header'],
+            objects = s['objects'])
+    return loc, files
+
 def generate_cpp_header_all(path, namespace, files):
     fin = os.path.join("templates", "api_all.h.mako")
     fout = os.path.join(path, "%s_all.h"%namespace)
@@ -21,57 +45,15 @@ def generate_cpp_header_all(path, namespace, files):
         files=files)
 
 def generate_cpp_headers(path, namespace, specs):
-    loc = 0
     cpp_path = os.path.join(path, "include")
-    util.makePath(cpp_path)
-    util.removeFiles(cpp_path, "*.h")
-    util.removeFiles(cpp_path, "*.txt")
-
-    files = []
-    for s in specs:
-        fin = os.path.join("templates", "api.h.mako")
-        filename = "%s_%s.h"%(namespace, s['name'])
-        files.append(filename)
-        fout = os.path.join(cpp_path, filename)
-
-        print("Generating %s..."%fout)
-        loc += util.makoWrite(
-            fin, fout,
-            x=namespace,
-            X=namespace.upper(),
-            Xx=namespace.title(),
-            name = s['name'],
-            header = s['header'],
-            objects = s['objects'])
-
+    loc, files = generate_code(cpp_path, namespace, specs, ".h")
     loc += generate_cpp_header_all(cpp_path, namespace, files)
     generate_cmake(cpp_path, namespace, files)
     return loc
 
 def generate_cpp_source(path, namespace, specs):
-    loc = 0
     cpp_path = os.path.join(path, "source")
-    util.makePath(cpp_path)
-    util.removeFiles(cpp_path, "*.cpp")
-    util.removeFiles(cpp_path, "*.txt")
-
-    files = []
-    for s in specs:
-        fin = os.path.join("templates", "api.cpp.mako")
-        filename = "%s_%s.cpp"%(namespace, s['name'])
-        files.append(filename)
-        fout = os.path.join(cpp_path, filename)
-
-        print("Generating %s..."%fout)
-        loc += util.makoWrite(
-            fin, fout,
-            x=namespace,
-            X=namespace.upper(),
-            Xx=namespace.title(),
-            name = s['name'],
-            header = s['header'],
-            objects = s['objects'])
-
+    loc, files = generate_code(cpp_path, namespace, specs, ".cpp")
     generate_cmake(cpp_path, namespace, files)
     return loc
 
