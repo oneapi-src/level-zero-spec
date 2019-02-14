4b5568e22cc72ce4e41978a0995b34616e5cf3e5
Zack Waters <zswaters@gmail.com>
2019-02-14 13:48:07 -0800

    Add const to dispatch args.

--- core/include/xe_module.h
+++ core/include/xe_module.h
@@ -496,7 +496,7 @@ xe_result_t __xecall
     xe_command_list_handle_t hCommandList,          ///< [in] handle of the command list
     xe_function_handle_t hFunction,                 ///< [in] handle of the function object
     xe_function_args_handle_t hFunctionArgs,        ///< [in] handle to function arguments buffer.
-    xe_dispatch_function_arguments_t* pDispatchFuncArgs,///< [in] dispatch function arguments.
+    const xe_dispatch_function_arguments_t* pDispatchFuncArgs,  ///< [in] dispatch function arguments.
     xe_event_handle_t hEvent                        ///< [in][optional] handle of the event to signal on completion
     );
 
@@ -527,7 +527,7 @@ xe_result_t __xecall
     xe_command_graph_handle_t hCommandGraph,        ///< [in] handle of the command graph
     xe_function_handle_t hFunction,                 ///< [in] handle of the function object
     xe_function_args_handle_t hFunctionArgs,        ///< [in] handle to function arguments buffer.
-    xe_dispatch_function_arguments_t* pDispatchFuncArgs,///< [in] dispatch function arguments.
+    const xe_dispatch_function_arguments_t* pDispatchFuncArgs,  ///< [in] dispatch function arguments.
     xe_event_handle_t hEvent                        ///< [in][optional] handle of the event to signal on completion
     );
 
