6e12c8bb5304a0b22b5bc94acbb98707166c220b
Brandon Fliflet <brandon.fliflet@intel.com>
2019-02-17 17:00:33 -0800

    encodeDispatchFunction now copies threadData to GeneralStateHeap for ATS

--- tests/unit_tests/cmdlist_ats.cpp
+++ tests/unit_tests/cmdlist_ats.cpp
@@ -1,6 +1,8 @@
 #include "runtime/gen12hp/hw_cmds.h"
 #include "runtime/gen12hp/hw_info.h"
+#include "runtime/indirect_heap/indirect_heap.h"
 #include "cmdlist_hw.inl"
+#include "module.h"
 #include "igfxfmid.h"
 
 namespace xe {
@@ -12,6 +14,10 @@ xe_result_t CommandListHw<IGFX_GEN12_CORE>::encodeDispatchFunction(xe_function_h
                                                                    xe_event_handle_t hEvent) {
     using GfxFamily = typename OCLRT::GfxFamilyMapper<IGFX_GEN12_CORE>::GfxFamily;
     using COMPUTE_WALKER = typename GfxFamily::COMPUTE_WALKER;
+    auto function = Function::fromHandle(hFunction);
+    assert(function);
+    auto functionArgs = FunctionArgs::fromHandle(hFunctionArgs);
+    assert(functionArgs);
 
     COMPUTE_WALKER cmd = GfxFamily::cmdInitGpgpuWalker;
     cmd.setThreadGroupIdXDimension(pDispatchFuncArgs->groupCountX);
@@ -19,10 +25,63 @@ xe_result_t CommandListHw<IGFX_GEN12_CORE>::encodeDispatchFunction(xe_function_h
     cmd.setThreadGroupIdZDimension(pDispatchFuncArgs->groupCountZ);
     cmd.setEmitLocalId(0u);
     cmd.setExecutionMask(0xfffffffful);
-    cmd.setSimdSize(COMPUTE_WALKER::SIMD_SIZE_SIMD32);
 
+    auto simdSize = function->getSimdSize();
+    auto simdSizeOp = 
+        COMPUTE_WALKER::SIMD_SIZE_SIMD32 * (1 & (simdSize >> 5)) |
+        COMPUTE_WALKER::SIMD_SIZE_SIMD16 * (1 & (simdSize >> 4)) |
+        COMPUTE_WALKER::SIMD_SIZE_SIMD8 * (1 & (simdSize >> 3));
+    cmd.setSimdSize(static_cast<COMPUTE_WALKER::SIMD_SIZE>(simdSizeOp));
+
+    // Copy the kernel to indirect heap
+    // TODO: Allocate kernel in graphics memory to avoid the CPU copy
+    {
+        auto sizeISA = function->getIsaSize();
+        auto ptrISA = function->getIsaHostMem();
+
+        constexpr auto INSTRUCTION_HEAP = OCLRT::IndirectHeap::SURFACE_STATE + 1;
+        auto heap = this->indirectHeaps[INSTRUCTION_HEAP];
+        assert(heap);
+
+        auto ptr = heap->getSpace(sizeISA);
+        assert(ptr);
+        memcpy(ptr, ptrISA, sizeISA);
+
+        auto &idd = cmd.getInterfaceDescriptor();
+        idd.setKernelStartPointer(static_cast<uint32_t>(reinterpret_cast<uint64_t>(ptr)));
+        idd.setKernelStartPointerHigh(0u);
+    }
+
+    // Copy the threadData to the indirect heap
+    {
+        auto heap = indirectHeaps[OCLRT::IndirectHeap::GENERAL_STATE];
+        assert(heap);
+
+        auto sizeCrossThreadData = static_cast<uint32_t>(functionArgs->getCrossThreadDataSize());
+        auto sizePerThreadData = static_cast<uint32_t>(functionArgs->getPerThreadDataSize());
+        auto sizeThreadData = sizePerThreadData + sizeCrossThreadData; 
+        auto ptr = heap->getSpace(sizeThreadData);
+        assert(ptr);
+        auto offset = ptrDiff(ptr, heap->getCpuBase());
+        assert(offset + sizeThreadData <= heap->getMaxAvailableSpace());
+
+        memcpy(ptr, functionArgs->getPerThreadDataHostMem(), sizePerThreadData);
+        ptr = ptrOffset(ptr, sizePerThreadData);
+        memcpy(ptr, functionArgs->getCrossThreadDataHostMem(), sizeCrossThreadData);
+
+        cmd.setIndirectDataLength(sizeThreadData);
+        cmd.setIndirectDataStartAddress(static_cast<uint32_t>(offset));
+    }
+
+    // Attach Function residency to our CommandList residency
+    {
+        //TODO: functionArgs->getResidencyContainer();
+    }
+
+    // Commit our command to the commandStream
     auto buffer = commandStream->getSpace(sizeof(cmd));
     *(decltype(cmd) *)buffer = cmd;
+
     return XE_RESULT_SUCCESS;
 }
 
--- tests/unit_tests/test_cmdlist_encodedispatchfunction.cpp
+++ tests/unit_tests/test_cmdlist_encodedispatchfunction.cpp
@@ -6,7 +6,10 @@
 #include "mock_device.h"
 #include "mock_event.h"
 #include "mock_memory_manager.h"
+#include "mock_module.h"
+#include "mock_module_precompiled.h"
 #include "runtime/command_stream/linear_stream.h"
+#include "runtime/indirect_heap/indirect_heap.h"
 #include "unit_tests/gen_common/gen_cmd_parse.h"
 #include "gmock/gmock.h"
 
@@ -43,31 +46,56 @@ TEST(xeCommandListEncodeDispatchFunction, redirectsToCmdListObject) {
     EXPECT_EQ(XE_RESULT_SUCCESS, result);
 }
 
-using CommandListEncodeDispatchFunction = ::testing::Test;
+struct CommandListEncodeDispatchFunction : public ::testing::Test {
 
-ATSTEST_F(CommandListEncodeDispatchFunction, addsWalkerToCommandStream) {
+    CommandListEncodeDispatchFunction() {
+    }
+
+    void SetUp() override {
+        EXPECT_CALL(device, getMemoryManager())
+            .WillRepeatedly(Return(&memoryManager));
+        EXPECT_CALL(memoryManager, allocateDeviceMemory(_)).Times(AnyNumber());
+        EXPECT_CALL(memoryManager, freeMemory(_)).Times(AnyNumber());
+
+        buffer1 = memoryManager.allocateDeviceMemory(16384);
+        buffer2 = memoryManager.allocateDeviceMemory(16384);
+
+        commandList = whitebox_cast(CommandList::create(productFamily, &device));
+        ASSERT_NE(nullptr, commandList->commandStream);
+
+        dispatchFunctionArguments.groupCountX = 1u;
+        dispatchFunctionArguments.groupCountY = 2u;
+        dispatchFunctionArguments.groupCountZ = 3u;
+
+        std::string deviceName = "Gen12HPcore";
+        function = new PrecompiledFunctionMock("MemcpyBytes", deviceName);
+        functionArgs = new PrecompiledFunctionArgsMock(function, {&buffer1, &buffer2});
+    }
+
+    void TearDown() override {
+        delete functionArgs;
+        delete function;
+        memoryManager.freeMemory(buffer1);
+        memoryManager.freeMemory(buffer2);
+    }
+
     Mock<Device> device;
     Mock<MemoryManager> memoryManager;
-    EXPECT_CALL(device, getMemoryManager())
-        .WillRepeatedly(Return(&memoryManager));
-    EXPECT_CALL(memoryManager, allocateDeviceMemory(_)).Times(AnyNumber());
-    EXPECT_CALL(memoryManager, freeMemory(_)).Times(AnyNumber());
+    WhiteBox<::xe::CommandList> *commandList = nullptr;
 
+    PrecompiledFunctionMock *function = nullptr;
+    PrecompiledFunctionArgsMock *functionArgs = nullptr;
+    xe_dispatch_function_arguments_t dispatchFunctionArguments;
 
-    auto commandList = whitebox_cast(CommandList::create(productFamily, &device));
-    ASSERT_NE(nullptr, commandList->commandStream);
+    GraphicsAllocation *buffer1 = nullptr;
+    GraphicsAllocation *buffer2 = nullptr;
+};
+
+ATSTEST_F(CommandListEncodeDispatchFunction, addsWalkerToCommandStream) {
     auto usedSpaceBefore = commandList->commandStream->getUsed();
 
-    xe_function_handle_t function = {};
-    xe_function_args_handle_t functionArgs = {};
-    xe_dispatch_function_arguments_t dispatchFunctionArguments = {};
-
-    dispatchFunctionArguments.groupCountX = 1u;
-    dispatchFunctionArguments.groupCountY = 2u;
-    dispatchFunctionArguments.groupCountZ = 3u;
-
-    auto result = commandList->encodeDispatchFunction(function,
-                                                      functionArgs,
+    auto result = commandList->encodeDispatchFunction(function->toHandle(),
+                                                      functionArgs->toHandle(),
                                                       &dispatchFunctionArguments,
                                                       nullptr);
     ASSERT_EQ(XE_RESULT_SUCCESS, result);
@@ -80,6 +108,8 @@ ATSTEST_F(CommandListEncodeDispatchFunction, addsWalkerToCommandStream) {
                                                       ptrOffset(commandList->commandStream->getCpuBase(), 0),
                                                       usedSpaceAfter));
     using COMPUTE_WALKER = typename FamilyType::COMPUTE_WALKER;
+    using INTERFACE_DESCRIPTOR_DATA = typename FamilyType::INTERFACE_DESCRIPTOR_DATA;
+
     auto itor = find<COMPUTE_WALKER *>(cmdList.begin(), cmdList.end());
     ASSERT_NE(cmdList.end(), itor);
 
@@ -92,6 +122,57 @@ ATSTEST_F(CommandListEncodeDispatchFunction, addsWalkerToCommandStream) {
         EXPECT_EQ(cmd->getEmitLocalId(), 0u);
         EXPECT_NE(cmd->getExecutionMask(), 0u);
         EXPECT_EQ(cmd->getSimdSize(), COMPUTE_WALKER::SIMD_SIZE_SIMD32);
+
+        EXPECT_NE(cmd->getIndirectDataLength(), 0u);
+
+        auto &idd = cmd->getInterfaceDescriptor();
+        EXPECT_NE(idd.getKernelStartPointer(), 0u);
+        EXPECT_EQ(idd.getKernelStartPointerHigh(), 0u);
+        EXPECT_EQ(idd.getSamplerCount(), INTERFACE_DESCRIPTOR_DATA::SAMPLER_COUNT_NO_SAMPLERS_USED);
+        EXPECT_EQ(idd.getSamplerStatePointer(), 0u);
+        EXPECT_EQ(idd.getBindingTableEntryCount(), 0u);
+        EXPECT_EQ(idd.getBindingTablePointer(), 0u);
+        EXPECT_EQ(idd.getNumberOfThreadsInGpgpuThreadGroup(), 0u);
+        EXPECT_EQ(idd.getSharedLocalMemorySize(), INTERFACE_DESCRIPTOR_DATA::SHARED_LOCAL_MEMORY_SIZE_ENCODES_0K);
+        EXPECT_EQ(idd.getBarrierEnable(), 0u);
+        EXPECT_EQ(idd.getThreadGroupDispatchSize(), 0u);
+    }
+}
+
+ATSTEST_F(CommandListEncodeDispatchFunction, copiesThreadDataToGeneralStateHeap) {
+    auto result = commandList->encodeDispatchFunction(function->toHandle(),
+                                                      functionArgs->toHandle(),
+                                                      &dispatchFunctionArguments,
+                                                      nullptr);
+    ASSERT_EQ(XE_RESULT_SUCCESS, result);
+
+    auto usedSpaceAfter = commandList->commandStream->getUsed();
+
+    GenCmdList cmdList;
+    ASSERT_TRUE(FamilyType::PARSE::parseCommandBuffer(cmdList,
+                                                      ptrOffset(commandList->commandStream->getCpuBase(), 0),
+                                                      usedSpaceAfter));
+    using COMPUTE_WALKER = typename FamilyType::COMPUTE_WALKER;
+    using INTERFACE_DESCRIPTOR_DATA = typename FamilyType::INTERFACE_DESCRIPTOR_DATA;
+
+    auto itor = find<COMPUTE_WALKER *>(cmdList.begin(), cmdList.end());
+    ASSERT_NE(cmdList.end(), itor);
+
+    {
+        auto cmd = genCmdCast<COMPUTE_WALKER *>(*itor);
+
+        auto indirectDataLength = functionArgs->getPerThreadDataSize() +
+                                  functionArgs->getCrossThreadDataSize();
+        EXPECT_LE(cmd->getIndirectDataLength(), indirectDataLength);
+
+        cmd->getIndirectDataLength();
+        cmd->getIndirectDataStartAddress();
+        auto heap = commandList->indirectHeaps[CommandList::GENERAL_STATE];
+        
+        auto ptrHeap = ptrOffset(heap->getCpuBase(), cmd->getIndirectDataStartAddress());
+        EXPECT_EQ(memcmp(ptrHeap, functionArgs->getPerThreadDataHostMem(), functionArgs->getPerThreadDataSize()), 0u);
+        ptrHeap = ptrOffset(ptrHeap, functionArgs->getPerThreadDataSize());
+        EXPECT_EQ(memcmp(ptrHeap, functionArgs->getCrossThreadDataHostMem(), functionArgs->getCrossThreadDataSize()), 0u);
     }
 }
 
