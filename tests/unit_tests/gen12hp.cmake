cdf9e4cab22636e858edd3ee30dedc56011e05b7
Brandon Fliflet <brandon.fliflet@intel.com>
2019-02-20 14:33:04 -0800

    Cleaning up HW specific CMake files for Linux compatibility.

--- tests/unit_tests/gen12hp.cmake
+++ tests/unit_tests/gen12hp.cmake
@@ -1,56 +1,16 @@
-set(HW_TARGET gen12hp)
-
-set(HW_SOURCES
-    ${COMPUTE_RUNTIME_GEN12HP}
-    cmdlist_ats.cpp
-    cmdqueue_ats.cpp
-    gen12hp.cmake
-)
-
-set(HW_SOURCES_ULT
-    ${COMPUTE_RUNTIME_ULT_GEN12HP}
-    ${HW_SOURCES}
-)
-
 if(SUPPORT_GEN12HP)
-    add_library(${HW_TARGET}
-        OBJECT
-            ${HW_SOURCES}
+    set(HW_SOURCES
+        ${HW_SOURCES}
+        ${COMPUTE_RUNTIME_GEN12HP}
+        cmdlist_ats.cpp
+        cmdqueue_ats.cpp
+        gen12hp.cmake
     )
 
-    target_link_libraries(${HW_TARGET}
-        compute_runtime_lib
-        compute_runtime_lib_full
-    )
-
-    target_include_directories(${HW_TARGET} 
-        PRIVATE 
-            ${COMPUTE_RUNTIME_INCLUDES}
-    )
-
-    target_compile_definitions(${HW_TARGET} 
-        PRIVATE 
-            ${COMPUTE_RUNTIME_DEFINITIONS}
-    )
-
-    add_library(${HW_TARGET}_ult
-        OBJECT
-            ${HW_SOURCES_ULT}
-    )
-
-    target_link_libraries(${HW_TARGET}_ult
-        compute_runtime_mockable
-        compute_runtime_mockable_extra
-    )
-
-    set(HW_LIBS
-        ${HW_LIBS}
-        ${HW_TARGET}
-    )
-
-    set(HW_LIBS_ULT
-        ${HW_LIBS_ULT}
-        ${HW_TARGET}_ult
+    set(HW_SOURCES_ULT
+        ${HW_SOURCES_ULT}
+        ${COMPUTE_RUNTIME_ULT_GEN12HP}
+        ${HW_SOURCES}
     )
 
     set(DEFAULT_PRODUCT_FAMILY IGFX_TIGERLAKE_HP)
--- tests/unit_tests/gen8.cmake
+++ tests/unit_tests/gen8.cmake
@@ -1,57 +1,17 @@
-set(HW_TARGET gen8)
-
-set(HW_SOURCES
-    ${COMPUTE_RUNTIME_GEN8}
-    cmdlist_bdw.cpp
-    cmdqueue_bdw.cpp
-    gen8.cmake
-)
-
-set(HW_SOURCES_ULT
-    ${COMPUTE_RUNTIME_ULT_GEN8}
-    ${HW_SOURCES}
-)
-
 if(SUPPORT_GEN8)
-    add_library(${HW_TARGET}
-        OBJECT
-            ${HW_SOURCES}
+    set(HW_SOURCES
+        ${HW_SOURCES}
+        ${COMPUTE_RUNTIME_GEN8}
+        cmdlist_bdw.cpp
+        cmdqueue_bdw.cpp
+        gen8.cmake
     )
 
-    target_link_libraries(${HW_TARGET}
-        compute_runtime_lib
-        compute_runtime_lib_full
+    set(HW_SOURCES_ULT
+        ${HW_SOURCES_ULT}
+        ${COMPUTE_RUNTIME_ULT_GEN8}
+        ${HW_SOURCES}
     )
 
-    target_include_directories(${HW_TARGET} 
-        PRIVATE 
-            ${COMPUTE_RUNTIME_INCLUDES}
-    )
-
-    target_compile_definitions(${HW_TARGET} 
-        PRIVATE 
-            ${COMPUTE_RUNTIME_DEFINITIONS}
-    )
-
-    add_library(${HW_TARGET}_ult
-        OBJECT
-            ${HW_SOURCES_ULT}
-    )
-
-    target_link_libraries(${HW_TARGET}_ult
-        compute_runtime_mockable
-        compute_runtime_mockable_extra
-    )
-
-    set(HW_LIBS
-        ${HW_LIBS}
-        ${HW_TARGET}
-    )
-
-    set(HW_LIBS_ULT
-        ${HW_LIBS_ULT}
-        ${HW_TARGET}_ult
-    )
-
-    set(DEFAULT_PRODUCT_FAMILY IGFX_ICELAKE_LP)
+    set(DEFAULT_PRODUCT_FAMILY IGFX_BROADWELL)
 endif()
