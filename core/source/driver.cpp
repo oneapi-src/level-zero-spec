f245b1a2178253524010d73fb7b8c856f7008e64
Jaroslaw Chodor <jaroslaw.chodor@intel.com>
2019-03-19 11:58:56 +0100

    Basic (single-device) getDeviceUniqueIds
    Compute-samples rely on this API being present
    for device discovery

--- core/source/driver.cpp
+++ core/source/driver.cpp
@@ -1,9 +1,13 @@
 #include "device.h"
 #include "driver.h"
+#include "runtime/helpers/string.h"
 #include "runtime/platform/platform.h"
 #include <cassert>
 
 namespace L0 {
+
+constexpr xe_device_uuid_t singleDeviceId{"single_device"}; // replace with really unique ids
+                                                            // once multidevice support gets added
 
 struct DriverImp : public Driver {
     xe_result_t init(xe_init_flag_t) override {
@@ -30,8 +34,12 @@ struct DriverImp : public Driver {
     }
 
     xe_result_t getDeviceUniqueIds(uint32_t count,
-                                   xe_device_uuid_t* pUniqueIds) override {
-        return XE_RESULT_ERROR_UNSUPPORTED;
+                                   xe_device_uuid_t *pUniqueIds) override {
+        if (count < 1) {
+            return XE_RESULT_ERROR_INVALID_PARAMETER;
+        }
+        memcpy_s(&pUniqueIds[0], sizeof(xe_device_uuid_t), &singleDeviceId, sizeof(singleDeviceId));
+        return XE_RESULT_SUCCESS;
     }
 
     xe_result_t getVersion(uint32_t *version) override {
