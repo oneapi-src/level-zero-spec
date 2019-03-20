6f7253b48c5173e807345ed98ab56dcf5d20051b
Artur Harasimiuk <artur.harasimiuk@intel.com>
2019-03-21 00:30:21 +0100

    ci: enable windows-64 builds
    Change-Id: If146b03b801832a3e16bbb03a6b9ebde0f6deb26
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

--- ci/jenkins/pipeline.groovy
+++ ci/jenkins/pipeline.groovy
@@ -25,30 +25,46 @@ def lokiStage = { name, body ->
   currentStage = null
 }
 
-def lokiBuild = { name, body ->
-	if(!buildData[currentStage].containsKey('builds')) {
-		buildData[currentStage]['builds'] = [:]
-	}
-	if(isUnix()) {
-		sh "ccache -z"
-	}
-	buildData[currentStage]['builds'][name] = [:]
-	buildData[currentStage]['builds'][name]['start'] = System.currentTimeMillis()
-	body()
-	if (!(buildData[currentStage]['builds'][name].containsKey('node')) && (env.NODE_NAME)) {
-		buildData[currentStage]['builds'][name]['node'] = env.NODE_NAME
-	}
-	buildData[currentStage]['builds'][name]['end'] = System.currentTimeMillis()
-
-
-	if(isUnix()) {
-		def tmp = sh script: "ccache -s | grep 'hit rate' | cut -d ' ' -f 4-", returnStdout: true
-		buildData[currentStage]['builds'][name]['ccache'] = tmp.trim()
+def lokiNode = { name, body ->
+	return {
+		echo "entering node ${name}"
+		node(name) {
+			echo "acquired node ${env.NODE_NAME}"
+			body()
+		}
 	}
 }
 
-node("loki-controller") {
-	try {
+def lokiBuild = { name, body ->
+	return {
+		try {
+			if(!buildData[currentStage].containsKey('builds')) {
+				buildData[currentStage]['builds'] = [:]
+			}
+			if(isUnix()) {
+				sh "ccache -z"
+			}
+			buildData[currentStage]['builds'][name] = [:]
+			buildData[currentStage]['builds'][name]['start'] = System.currentTimeMillis()
+			body()
+			buildData[currentStage]['builds'][name]['passed'] = true
+		} finally {
+			if (!(buildData[currentStage]['builds'][name].containsKey('node')) && (env.NODE_NAME)) {
+				buildData[currentStage]['builds'][name]['node'] = env.NODE_NAME
+			}
+			buildData[currentStage]['builds'][name]['end'] = System.currentTimeMillis()
+
+
+			if(isUnix()) {
+				def tmp = sh script: "ccache -s | grep 'hit rate' | cut -d ' ' -f 4-", returnStdout: true
+				buildData[currentStage]['builds'][name]['ccache'] = tmp.trim()
+			}
+		}
+	}
+}
+
+try {
+	node("loki-common") {
 		lokiStage("checkout") {
 			checkout changelog: false, poll: false,
 				scm: [$class: 'GitSCM',
@@ -66,9 +82,35 @@ node("loki-controller") {
 						refspec: "+${GERRIT_REFSPEC}:${gerritLocalBranch}"
 					]]]
 		}
-		lokiStage("sync manifest") {
-			sh """\
+    lokiStage("lint") {
+			echo "TBD"
+    }
+	}
+
+	lokiStage('build') {
+		lokiBuilds = [:]
+		lokiBuilds['linux'] = lokiNode("loki-controller") {
+			lokiBuild('linux') {
+				checkout changelog: false, poll: false,
+					scm: [$class: 'GitSCM',
+						branches: [[name: "${GERRIT_PATCHSET_REVISION}"]],
+						doGenerateSubmoduleConfigurations: false,
+						extensions: [
+							[$class: 'CleanCheckout'],
+							// [$class: 'RelativeTargetDirectory', relativeTargetDir: 'loki'],
+							[$class: 'CloneOption', noTags: true, reference: '', shallow: false, honorRefspec: true],
+							[$class: 'PruneStaleBranch']],
+						submoduleCfg: [],
+						userRemoteConfigs: [[
+							credentialsId: '0f565f2a-f10f-421d-ac50-3b169adf5f06',
+							url: "${gerritUrl}",
+							refspec: "+${GERRIT_REFSPEC}:${gerritLocalBranch}"
+						]]]
+
+				withEnv(["IREPO_CACHE_DIR=$HOME/.irepo"]) {
+					sh """\
 ~/irepo/irepo select -d . third_party/linux_embargo.yml
+
 ~/irepo/irepo ignore-folder extended
 ~/irepo/irepo ignore-folder scripts
 ~/irepo/irepo ignore-folder .git
@@ -80,11 +122,10 @@ node("loki-controller") {
 ~/irepo/irepo ignore-folder samples
 
 ~/irepo/irepo sync --clean --delete-unknown-content
+ls -la
 """
-		}
+				}
 
-		lokiStage('build') {
-			lokiBuild('linux') {
 				def image = docker.image("${env.DOCKER_REGISTRY}/neo-build-ubuntu18:2")
 				def workDir = sh script: "(cd .. && pwd)", returnStdout: true
 				def buildId = "${env.BUILD_NUMBER}"
@@ -99,11 +140,64 @@ cmake -GNinja .. -DCMAKE_BUILD_TYPE=Release -DLOKI_VERSION_BUILD=${buildId}
 cmake --build . --config Release --clean-first --target package
 """
 				}
-			}
+				archiveArtifacts "build/*.deb"
+			}()
 		}
 
+		lokiBuilds['windows-64'] = lokiNode('loki-windows') {
+			lokiBuild('windows-64') {
+				checkout changelog: false, poll: false,
+					scm: [$class: 'GitSCM',
+						branches: [[name: "${GERRIT_PATCHSET_REVISION}"]],
+						doGenerateSubmoduleConfigurations: false,
+						extensions: [
+							[$class: 'CleanCheckout'],
+							// [$class: 'RelativeTargetDirectory', relativeTargetDir: 'loki'],
+							[$class: 'CloneOption', noTags: true, reference: '', shallow: false, honorRefspec: true],
+							[$class: 'PruneStaleBranch']],
+						submoduleCfg: [],
+						userRemoteConfigs: [[
+							credentialsId: '0f565f2a-f10f-421d-ac50-3b169adf5f06',
+							url: "${gerritUrl}",
+							refspec: "+${GERRIT_REFSPEC}:${gerritLocalBranch}"
+						]]]
+
+				echo "irepo cache dir = ${BASE}\\.irepo"
+				withEnv(["IREPO_CACHE_DIR=${BASE}\\.irepo"]) {
+					bat """\
+call c:\\irepo\\irepo select third_party\\windows_embargo.yml
+
+call c:\\irepo\\irepo ignore-folder extended
+call c:\\irepo\\irepo ignore-folder scripts
+call c:\\irepo\\irepo ignore-folder .git
+call c:\\irepo\\irepo ignore-folder ci
+call c:\\irepo\\irepo ignore-folder icd_loader
+call c:\\irepo\\irepo ignore-folder tests
+call c:\\irepo\\irepo ignore-folder images
+call c:\\irepo\\irepo ignore-folder core
+call c:\\irepo\\irepo ignore-folder samples
+
+call c:\\irepo\\irepo sync --clean --delete-unknown-content
+"""
+				}
+				def buildId = "${env.BUILD_NUMBER}"
+				if(params.containsKey("COMMON_BUILD_ID")) {
+					buildId = "${COMMON_BUILD_ID}"
+				}
+				bat """
+md build
+cd build
+cmake -G "Visual Studio 15 2017 Win64" .. -DCMAKE_BUILD_TYPE=Release -DLOKI_VERSION_BUILD=${buildId} -DLOKI_CPACK_GENERATOR="ZIP"
+cmake --build . --config Release --clean-first --target PACKAGE
+"""
+				archiveArtifacts "build/*.zip"
+			}()
+		}
+
+		parallel lokiBuilds
+	}
+
 		lokiStage('publish') {
-			archiveArtifacts "build/*.deb"
 
 			if(reportBuildStatus) {
 				lock('loki-gerrit') {
@@ -130,12 +224,12 @@ cmake --build . --config Release --clean-first --target package
 			}
 		}
 	} finally {
-		dir("tmp") {
-			writeFile file: 'build_data.json', text: JsonOutput.prettyPrint(JsonOutput.toJson(buildData))
-			archiveArtifacts "build_data.json"
-			deleteDir()
+		node("loki-common") {
+			dir("tmp") {
+				writeFile file: 'build_data.json', text: JsonOutput.prettyPrint(JsonOutput.toJson(buildData))
+				archiveArtifacts "build_data.json"
+				deleteDir()
+			}
 		}
 	}
-}
-
 }
