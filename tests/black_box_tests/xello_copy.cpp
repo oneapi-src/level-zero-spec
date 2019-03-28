5a927f368f5419d9f456ecc07fcbc4256e2cc1be
Jaime Arteaga <jaime.a.arteaga.molina@intel.com>
2019-03-27 17:18:36 -0700

    Refactor xello_copy

--- tests/black_box_tests/xello_copy.cpp
+++ tests/black_box_tests/xello_copy.cpp
@@ -25,43 +25,32 @@ inline void validate(ResulT result, const char *message) {
 #define SUCCESS_OR_WARNING(CALL) validate<false>(CALL, #CALL)
 #define SUCCESS_OR_WARNING_BOOL(FLAG) validate<false>(!(FLAG), #FLAG)
 
-int main(int argc, char *argv[]) {
+void testAppendMemoryCopy(xe_device_handle_t &device,
+                          bool &syncRet, bool &validRet) {
     const size_t allocSize = 4096 + 7; // +7 to brake alignment and make it harder
-    xe_device_handle_t device0;
-    xe_device_properties_t device0Properties = {XE_DEVICE_PROPERTIES_VERSION_CURRENT};
-    xe_command_queue_handle_t cmdQueue;
-    xe_command_list_handle_t cmdList;
-    xe_mem_allocator_handle_t allocator;
-
-    if ((argc >= 2) && ((0 == strcmp(argv[1], "-v")) || (0 == strcmp(argv[1], "--verbose")))) {
-        verbose = true;
-    }
-
-    SUCCESS_OR_TERMINATE(xeDriverInit(XE_INIT_FLAG_NONE));
-    xe_device_uuid_t deviceUniqueID = {};
-    SUCCESS_OR_TERMINATE(xeDriverGetDevice(&deviceUniqueID, &device0));
-    SUCCESS_OR_TERMINATE(xeDeviceGetProperties(device0, &device0Properties));
-    std::cout << device0Properties.device_name << std::endl;
-
-    xe_command_queue_desc_t cmdQueueDesc = {XE_COMMAND_QUEUE_DESC_VERSION_CURRENT};
-    cmdQueueDesc.ordinal = 0;
-    cmdQueueDesc.mode = XE_COMMAND_QUEUE_MODE_ASYNCHRONOUS;
-    SUCCESS_OR_TERMINATE(xeDeviceCreateCommandQueue(device0, &cmdQueueDesc, &cmdQueue));
-
-    xe_command_list_desc_t cmdListDesc = {XE_COMMAND_LIST_DESC_VERSION_CURRENT};
-    SUCCESS_OR_TERMINATE(xeDeviceCreateCommandList(device0, &cmdListDesc, &cmdList));
-
-    SUCCESS_OR_TERMINATE(xeCreateMemAllocator(&allocator));
-
     char *heapBuffer = new char[allocSize];
     void *xeBuffer = nullptr;
     char stackBuffer[allocSize];
 
-    SUCCESS_OR_TERMINATE(xeMemAlloc(allocator, device0,
+    xe_command_queue_handle_t cmdQueue;
+    xe_command_list_handle_t cmdList;
+
+    xe_command_queue_desc_t cmdQueueDesc = {XE_COMMAND_QUEUE_DESC_VERSION_CURRENT};
+    cmdQueueDesc.ordinal = 0;
+    cmdQueueDesc.mode = XE_COMMAND_QUEUE_MODE_ASYNCHRONOUS;
+    SUCCESS_OR_TERMINATE(xeDeviceCreateCommandQueue(device, &cmdQueueDesc, &cmdQueue));
+
+    xe_command_list_desc_t cmdListDesc = {XE_COMMAND_LIST_DESC_VERSION_CURRENT};
+    SUCCESS_OR_TERMINATE(xeDeviceCreateCommandList(device, &cmdListDesc, &cmdList));
+
+    xe_mem_allocator_handle_t allocator;
+    SUCCESS_OR_TERMINATE(xeCreateMemAllocator(&allocator));
+
+    SUCCESS_OR_TERMINATE(xeMemAlloc(allocator, device,
                                     XE_DEVICE_MEM_ALLOC_FLAG_DEFAULT,
                                     allocSize, allocSize, &xeBuffer));
 
-    for (int i = 0; i < allocSize; ++i) {
+    for (size_t i = 0; i < allocSize; ++i) {
         heapBuffer[i] = i + 1;
     }
     memset(stackBuffer, 0, allocSize);
@@ -76,18 +65,35 @@ int main(int argc, char *argv[]) {
 
     SUCCESS_OR_TERMINATE(xeCommandListClose(cmdList));
     SUCCESS_OR_TERMINATE(xeCommandQueueExecuteCommandLists(cmdQueue, 1, &cmdList, nullptr));
-    auto synchronizationResult = xeCommandQueueSynchronize(cmdQueue, 1000 * 1000 /*1s*/);
-    SUCCESS_OR_WARNING(synchronizationResult);
+    syncRet = xeCommandQueueSynchronize(cmdQueue, 1000 * 1000 /*1s*/);
 
     // Validate stack and xe buffers have the original data from heapBuffer
-    bool outputValidationSuccessful = (0 == memcmp(heapBuffer, stackBuffer, sizeof(stackBuffer)));
+    validRet = (0 == memcmp(heapBuffer, stackBuffer, allocSize));
 
     delete[] heapBuffer;
     SUCCESS_OR_TERMINATE(xeMemFree(allocator, xeBuffer));
-
     SUCCESS_OR_TERMINATE(xeMemAllocatorDestroy(allocator));
     SUCCESS_OR_TERMINATE(xeCommandListDestroy(cmdList));
     SUCCESS_OR_TERMINATE(xeCommandQueueDestroy(cmdQueue));
+}
+
+int main(int argc, char *argv[]) {
+    xe_device_handle_t device0;
+    xe_device_properties_t device0Properties = {XE_DEVICE_PROPERTIES_VERSION_CURRENT};
+
+    if ((argc >= 2) && ((0 == strcmp(argv[1], "-v")) || (0 == strcmp(argv[1], "--verbose")))) {
+        verbose = true;
+    }
+
+    SUCCESS_OR_TERMINATE(xeDriverInit(XE_INIT_FLAG_NONE));
+    xe_device_uuid_t deviceUniqueID = {};
+    SUCCESS_OR_TERMINATE(xeDriverGetDevice(&deviceUniqueID, &device0));
+    SUCCESS_OR_TERMINATE(xeDeviceGetProperties(device0, &device0Properties));
+    std::cout << device0Properties.device_name << std::endl;
+
+    bool synchronizationResult;
+    bool outputValidationSuccessful;
+    testAppendMemoryCopy(device0, synchronizationResult, outputValidationSuccessful);
 
     bool aubMode = (XE_RESULT_NOT_READY == synchronizationResult);
     if (aubMode == false)
