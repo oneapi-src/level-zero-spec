4ea8c65ef079e847991f9f66f8d636eed7490e11
Apodaca, Michael <michael.apodaca@intel.com>
2019-05-10 09:32:28 -0700

    C++ wrapper update
    Add missing files from previous

--- scripts/generate_code.py
+++ scripts/generate_code.py
@@ -5,14 +5,14 @@ import util
 """
     generates c/c++ files from the specification documents
 """
-def generate_spec_cpp(path, namespace, tags, specs, meta, type):
+def mako_spec_cpp(path, namespace, tags, specs, meta, ext):
     loc = 0
-    template = "spec%s.mako"%type
+    template = "spec%s.mako"%ext
     fin = os.path.join("templates", template)
 
     files = []
     for s in specs:
-        filename = "%s_%s%s"%(namespace, s['name'], type)
+        filename = "%s_%s%s"%(namespace, s['name'], ext)
         files.append(filename)
         fout = os.path.join(path, filename)
 
@@ -32,16 +32,15 @@ def generate_spec_cpp(path, namespace, tags, specs, meta, type):
 """
     generates c/c++ files from the specification documents
 """
-def generate_section_cpp(path, namespace, tags, specs, meta, files, type):
-    loc = 0
-    template = "api%s.mako"%type
+def mako_api_cpp(path, namespace, tags, specs, meta, files, ext):
+    template = "api%s.mako"%ext
     fin = os.path.join("templates", template)
 
-    filename = "%s_api%s"%(namespace,type)
+    filename = "%s_api%s"%(namespace,ext)
     fout = os.path.join(path, filename)
 
     print("Generating %s..."%fout)
-    loc += util.makoWrite(
+    return util.makoWrite(
         fin, fout,
         section=os.path.basename(path),
         namespace=namespace,
@@ -49,17 +48,59 @@ def generate_section_cpp(path, namespace, tags, specs, meta, files, type):
         specs=specs,
         meta=meta,
         files=files)
-    return loc
 
 """
     generates c/c++ files from the specification documents
 """
-def generate_loader_cpp(path, section, namespace, tags, specs, meta, type):
-    template = "loader%s.mako"%type
+def mako_lib_cpp(path, namespace, tags, specs, meta):
+    loc = 0
+    template = "lib.cpp.mako"
+    fin = os.path.join("templates", template)
+
+    files = []
+    for s in specs:
+        filename = "%s_%s.cpp"%(namespace, s['name'])
+        files.append(filename)
+        fout = os.path.join(path, filename)
+
+        print("Generating %s..."%fout)
+        loc += util.makoWrite(
+            fin, fout,
+            name = s['name'],
+            header = s['header'],
+            objects = s['objects'],
+            section=os.path.basename(path),
+            namespace=namespace,
+            tags=tags,
+            specs=specs,
+            meta = meta)
+    return loc, files
+
+"""
+    generates c/c++ files from the specification documents
+"""
+def mako_cmake_cpp(path, namespace, files):
+    template = "lib.cmake.mako"
+    fin = os.path.join("templates", template)
+
+    filename = "CMakeLists.txt"
+    fout = os.path.join(path, filename)
+
+    print("Generating %s..."%fout)
+    return util.makoWrite(
+        fin, fout,
+        n=namespace,
+        files=files)
+
+"""
+    generates c/c++ files from the specification documents
+"""
+def mako_loader_cpp(path, section, namespace, tags, specs, meta):
+    template = "loader.cpp.mako"
     fin = os.path.join("templates", template)
 
     name = "%s_loader"%section
-    filename = "%s%s"%(name, type)
+    filename = "%s.cpp"%(name)
     fout = os.path.join(path, filename)
 
     print("Generating %s..."%fout)
@@ -74,13 +115,13 @@ def generate_loader_cpp(path, section, namespace, tags, specs, meta, type):
 """
     generates c/c++ files from the specification documents
 """
-def generate_validation_layer_cpp(path, section, namespace, tags, specs, meta, type):
+def mako_validation_layer_cpp(path, section, namespace, tags, specs, meta):
     dstpath = os.path.join(path, "validation")
-    template = "validation%s.mako"%type
+    template = "validation.cpp.mako"
     fin = os.path.join("templates", template)
 
     name = "%s_layer"%section
-    filename = "%s%s"%(name, type)
+    filename = "%s.cpp"%(name)
     fout = os.path.join(dstpath, filename)
 
     print("Generating %s..."%fout)
@@ -93,7 +134,7 @@ def generate_validation_layer_cpp(path, section, namespace, tags, specs, meta, t
         meta=meta)
 
 """
-    generates c/c++ include files from the specification documents
+    generates c/c++ files from the specification documents
 """
 def generate_api_cpp(path, namespace, tags, specs, meta):
     util.makePath(path)
@@ -101,22 +142,43 @@ def generate_api_cpp(path, namespace, tags, specs, meta):
     util.removeFiles(path, "*.hpp")
     util.removeFiles(path, "*.inl")
 
-    hloc, hfiles = generate_spec_cpp(path, namespace, tags, specs, meta, ".h")
-    hpploc, hppfiles = generate_spec_cpp(path, namespace, tags, specs, meta, ".hpp")
-    inlloc, inlfiles = generate_spec_cpp(path, namespace, tags, specs, meta, ".inl")
+    hloc, hfiles = mako_spec_cpp(path, namespace, tags, specs, meta, ".h")
+    hpploc, hppfiles = mako_spec_cpp(path, namespace, tags, specs, meta, ".hpp")
 
-    hloc += generate_section_cpp(path, namespace, tags, specs, meta, hfiles, ".h")
-    hpploc += generate_section_cpp(path, namespace, tags, specs, meta, hppfiles + inlfiles, ".hpp")
+    hloc += mako_api_cpp(path, namespace, tags, specs, meta, hfiles, ".h")
+    hpploc += mako_api_cpp(path, namespace, tags, specs, meta, hppfiles, ".hpp")
 
-    return hloc + hpploc + inlloc
+    return hloc + hpploc
+
+"""
+    generates c/c++ files from the specification documents
+"""
+def generate_lib_cpp(path, namespace, tags, specs, meta):
+    util.makePath(path)
+    util.removeFiles(path, "*.cpp")
+
+    loc, files = mako_lib_cpp(path, namespace, tags, specs, meta)
+
+    mako_cmake_cpp(path, namespace, files)
+
+    return loc
 
 """
 Entry-point:
-    generates all c/c++ code
+    generates api code
 """
 def generate_api(path, namespace, tags, specs, meta):
     loc = 0
     loc += generate_api_cpp(path, namespace, tags, specs, meta)
+    print("Generated %s lines of code.\n"%loc)
+
+"""
+Entry-point:
+    generates lib code
+"""
+def generate_lib(path, namespace, tags, specs, meta):
+    loc = 0
+    loc += generate_lib_cpp(path, namespace, tags, specs, meta)
     print("Generated %s lines of code.\n"%loc)
 
 """
@@ -128,7 +190,7 @@ def generate_loader(path, section, namespace, tags, specs, meta):
     util.makePath(dstpath)
 
     loc = 0
-    loc += generate_loader_cpp(dstpath, section, namespace, tags, specs, meta, ".cpp")
+    loc += mako_loader_cpp(dstpath, section, namespace, tags, specs, meta)
     return loc
 
 """
@@ -140,6 +202,6 @@ def generate_layers(path, section, namespace, tags, specs, meta):
     util.makePath(dstpath)
 
     loc = 0
-    loc += generate_validation_layer_cpp(dstpath, section, namespace, tags, specs, meta, ".cpp")
+    loc += mako_validation_layer_cpp(dstpath, section, namespace, tags, specs, meta)
     return loc
 
