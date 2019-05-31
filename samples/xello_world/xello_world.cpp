0dfa8e033c8830c564da30c869121d97b5ba5e00
Jaime Arteaga <jaime.a.arteaga.molina@intel.com>
2019-05-31 01:13:37 -0700

    Add verbosity and options for env vars to xello_world
    This helps testing the loaders better.
    Signed-off-by: Jaime Arteaga <jaime.a.arteaga.molina@intel.com>

--- samples/xello_world/xello_world.cpp
+++ samples/xello_world/xello_world.cpp
@@ -1,20 +1,27 @@
 #include <stdlib.h>
 #include "xe_api.h"
+#include <iostream>
 
 int main()
 {
+    //Enable only null driver by default
     putenv( const_cast<char *>("XE_ENABLE_NULL_DRIVER=1") );
-    putenv( const_cast<char *>("XE_ENABLE_LOADER_INTERCEPT=1") );
-    putenv( const_cast<char *>("XE_ENABLE_VALIDATION_LAYER=1") );
-    putenv( const_cast<char *>("XE_ENABLE_PARAMETER_VALIDATION=1") );
+    if (getenv(const_cast<char *>("XE_ENABLE_LOADER_INTERCEPT=0")) == nullptr)
+        putenv( const_cast<char *>("XE_ENABLE_LOADER_INTERCEPT=0") );
+    if (getenv(const_cast<char *>("XE_ENABLE_VALIDATION_LAYER=0")) == nullptr)
+        putenv( const_cast<char *>("XE_ENABLE_VALIDATION_LAYER=0") );
+    if (getenv(const_cast<char *>("XE_ENABLE_PARAMETER_VALIDATION=0")) == nullptr)
+        putenv( const_cast<char *>("XE_ENABLE_PARAMETER_VALIDATION=0") );
 
     // Initialize the driver
     xeInit( XE_INIT_FLAG_NONE );
+    std::cout << "Driver initialized\n";
 
     // Get the first device group
     uint32_t groupCount = 1;
     xe_device_group_handle_t hDeviceGroup = nullptr;
     xeGetDeviceGroups( &groupCount, &hDeviceGroup );
+    std::cout << "Number of device groups found: " << groupCount << "\n";
 
     xe_device_properties_t device_properties = {};
     xeDeviceGroupGetProperties( hDeviceGroup, &device_properties );
@@ -28,6 +35,7 @@ int main()
     xe_device_handle_t hDevice = nullptr;
     uint32_t deviceCount = 1;
     xeDeviceGroupGetDevices( hDeviceGroup, &deviceCount, &hDevice );
+    std::cout << "Number of devices found: " << groupCount << "\n";
 
     return 0;
 }
