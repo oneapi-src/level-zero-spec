89e07f934036b44ee5ef4a71a9038a6afa4ed114
Apodaca, Michael <michael.apodaca@intel.com>
2019-06-02 11:56:18 -0700

    Update xello_world.cpp
    C++ version

--- samples/xello_world/xello_world.cpp
+++ samples/xello_world/xello_world.cpp
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include "xe_api.h"
+#include "xe_api.hpp"
 #include <iostream>
 
 int main()
@@ -14,26 +14,24 @@ int main()
     }
 
     // Initialize the driver
-    xeInit( XE_INIT_FLAG_NONE );
+    xe::Init( xe::init_flag_t::NONE );
     std::cout << "Driver initialized\n";
 
     // Get the first device group
     uint32_t groupCount = 1;
-    xe_device_group_handle_t hDeviceGroup = nullptr;
-    xeDeviceGroupGet( &groupCount, &hDeviceGroup );
+    xe::DeviceGroup* pDeviceGroup = nullptr;
+    xe::DeviceGroup::Get( &groupCount, &pDeviceGroup );
 
-    xe_device_properties_t device_properties = {};
-    xeDeviceGroupGetProperties( hDeviceGroup, &device_properties );
+    auto device_properties = pDeviceGroup->GetProperties();
     // todo: to_string( device_properties );
 
-    xe_device_compute_properties_t compute_properties = {};
-    xeDeviceGroupGetComputeProperties( hDeviceGroup, &compute_properties );
+    auto compute_properties = pDeviceGroup->GetComputeProperties();
     // todo: to_string( compute_properties );
 
     // Get the first device within the device group
-    xe_device_handle_t hDevice = nullptr;
     uint32_t deviceCount = 1;
-    xeDeviceGet( hDeviceGroup, &deviceCount, &hDevice );
+    xe::Device* pDevice = nullptr;
+    xe::Device::Get( pDeviceGroup, &deviceCount, &pDevice );
 
     return 0;
 }
