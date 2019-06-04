ac371a169a4b0b62b271dbef297314c255cb2988
macabral <matias.a.cabral@intel.com>
2019-06-04 10:41:58 -0700

    Free ipc object after using in IPC APIs
    Relates-to:LOKI-226
    
    Change-Id: Ic415906ee28d3e74d182ca790fa376784d33de8b
    Signed-off-by: macabral <matias.a.cabral@intel.com>

--- source/core/memory.cpp
+++ source/core/memory.cpp
@@ -16,18 +16,24 @@ xe_result_t hostMemAlloc(xe_host_mem_alloc_flag_t flags, size_t size, size_t ali
 
 xe_result_t ipcCloseMemHandle(const void *ptr) {
     auto ipc = IPCMemoryManager::create();
-    return ipc->ipcCloseMemHandle(ptr);
+    xe_result_t result =  ipc->ipcCloseMemHandle(ptr);
+    delete ipc;
+    return result;
 }
 
-xe_result_t ipcGetMemHandle(const void *ptr, xe_ipc_mem_handle_t *pIpcHandle) {
+xe_result_t ipcGetMemHandle(const void *ptr, xe_ipc_mem_handle_t *pIpcHandle){
     auto ipc = IPCMemoryManager::create();
-    return ipc->ipcGetMemHandle(ptr, pIpcHandle);
+    xe_result_t result = ipc->ipcGetMemHandle(ptr, pIpcHandle);
+    delete ipc;
+    return result;
 }
 
 xe_result_t ipcOpenMemHandle(xe_device_handle_t hDevice, xe_ipc_mem_handle_t handle,
                              xe_ipc_memory_flag_t flags, void **ptr) {
     auto ipc = IPCMemoryManager::create();
-    return ipc->ipcOpenMemHandle(hDevice, handle, flags, ptr);
+    xe_result_t result = ipc->ipcOpenMemHandle(hDevice, handle, flags, ptr);
+    delete ipc;
+    return result;
 }
 
 xe_result_t deviceMemAlloc(xe_device_handle_t hDevice, xe_device_mem_alloc_flag_t flags,
