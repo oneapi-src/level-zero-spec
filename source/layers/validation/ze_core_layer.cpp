df779670250828f031de48cc6d68ff0736f46b61
Zack Waters <zswaters@gmail.com>
2020-01-28 16:21:26 -0800

    Fix small spelling issue.

--- source/layers/validation/ze_core_layer.cpp
+++ source/layers/validation/ze_core_layer.cpp
@@ -1547,7 +1547,7 @@ namespace layer
         ze_event_timestamp_type_t timestampType,        ///< [in] specifies timestamp type to query for that is associated with
                                                         ///< hEvent.
         void* dstptr                                    ///< [in,out] pointer to memory for where timestamp will be written to. The
-                                                        ///< size of tiemstamp is specified in the
+                                                        ///< size of timestamp is specified in the
                                                         ///< ::ze_event_timestamp_query_type_t definition.
         )
     {
