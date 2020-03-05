76c1c8d003922ab8f0e012f4f2bcff24dcf6a32c
Zack Waters <zswaters@gmail.com>
2020-03-05 10:31:14 -0800

    Rename XE to ZE in validation layer. One of the few places that we're not auto-generating these tags.

--- source/layers/validation/ze_layer.cpp
+++ source/layers/validation/ze_layer.cpp
@@ -16,10 +16,10 @@ namespace layer
     ///////////////////////////////////////////////////////////////////////////////
     context_t::context_t()
     {
-        enableParameterValidation = getenv_tobool( "XE_ENABLE_PARAMETER_VALIDATION" );
-        enableHandleLifetime = getenv_tobool( "XE_ENABLE_HANDLE_LIFETIME" );
-        enableMemoryTracker = getenv_tobool( "XE_ENABLE_MEMORY_TRACKER" );
-        enableThreadingValidation = getenv_tobool( "XE_ENABLE_THREADING_VALIDATION" );
+        enableParameterValidation = getenv_tobool( "ZE_ENABLE_PARAMETER_VALIDATION" );
+        enableHandleLifetime = getenv_tobool( "ZE_ENABLE_HANDLE_LIFETIME" );
+        enableMemoryTracker = getenv_tobool( "ZE_ENABLE_MEMORY_TRACKER" );
+        enableThreadingValidation = getenv_tobool( "ZE_ENABLE_THREADING_VALIDATION" );
     }
 
     ///////////////////////////////////////////////////////////////////////////////
